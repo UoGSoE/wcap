3375bfc70b4f749afcbb41ee290b64f4516d30a1
Ohffs <ohffsnotnow@gmail.com>
2025-11-04 19:02:35 +0000

    WIP

--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -18,5 +19,36 @@ class TestDataSeeder extends Seeder
             'email' => 'admin2x@example.com',
             'password' => Hash::make('secret'),
         ]);
+
+        $teamNames = [
+            'Apps and Data',
+            'Infrastructure',
+            'Resilience',
+            'Security',
+            'Front Desk',
+            'Other',
+        ];
+
+        $managers = [];
+        foreach ($teamNames as $teamName) {
+            $manager = User::factory()->create([
+                'username' => strtolower($teamName) . '2x',
+                'email' => 'manager.' . strtolower($teamName) . '2x@example.com',
+                'password' => Hash::make('secret'),
+            ]);
+            $team = Team::factory()->create([
+                'name' => $teamName,
+                'manager_id' => $manager->id,
+            ]);
+            $managers[$teamName] = $manager;
+
+            foreach (range(1, 10) as $i) {
+                $user = User::factory()->create([
+                    'username' => 'user' . strtolower($teamName) . '1x' . $i,
+                    'password' => Hash::make('secret'),
+                ]);
+                $user->teams()->attach($team);
+            }
+        }
     }
 }
