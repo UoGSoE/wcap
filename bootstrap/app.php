ade795aca45ed51197a9cf282195ec0cf8632b28
Ohffs <ohffsnotnow@gmail.com>
2026-03-12 10:14:00 +0000

    Add sentry and fix typo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'manager' => \App\Http\Middleware\ManagerMiddleware::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        Integration::handles($exceptions);
     })->create();
