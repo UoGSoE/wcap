46611324c99be03cce2d9ab46d44465cfdc48ac1
Ohffs <ohffsnotnow@gmail.com>
2026-03-18 13:05:38 +0000

    Add trustproxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'abilities' => CheckForAnyAbility::class,
             'manager' => ManagerMiddleware::class,
         ]);
+        $middleware->trustProxies(at: [
+            '*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
