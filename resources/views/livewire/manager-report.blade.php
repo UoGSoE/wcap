ba4146b02f6847e97886fd97c83c7c2b30a0f542
Ohffs <ohffsnotnow@gmail.com>
2026-07-29 10:54:53 +0100

    Wording

--- resources/views/livewire/manager-report.blade.php
+++ resources/views/livewire/manager-report.blade.php
@@ -171,7 +171,7 @@
         <flux:tab.panel name="coverage">
             <flux:subheading>Location coverage at a glance</flux:subheading>
             <flux:text class="text-sm text-zinc-600 dark:text-zinc-400 mt-2 mb-6">
-                Gray cells indicate at least one person supporting that location. Gaps mean no coverage.
+                Gray cells indicate at least one person supporting that area. Gaps mean no coverage.
             </flux:text>
 
             <div class="overflow-x-auto">
